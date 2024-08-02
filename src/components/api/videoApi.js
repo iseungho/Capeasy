86ecc4c8954895938eac133724e49ef8b9ef4730
iseungho <ghehf51@naver.com>
2024-08-03 00:23:14 +0900

    feat : AWS 서버 연결

--- src/components/api/videoApi.js
+++ src/components/api/videoApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_SERVER_HOST = 'http://192.168.0.100';
+export const API_SERVER_HOST = 'http://52.78.93.135';
 const prefix = `${API_SERVER_HOST}/api`;
 
 export const postVideo = async (video) => {
