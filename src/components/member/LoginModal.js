33e950e7b5f7c39fcb91f1e57ce74cfa38d7bd26
iseungho <ghehf51@naver.com>
2024-09-10 16:08:28 +0900

    feat: 로그인 회원가입 모달 통합

--- src/components/member/LoginModal.js
+++ src/components/member/LoginModal.js
@@ -1,29 +1,40 @@
 import React, { useState } from "react";
 import useCustomLogin from "../../hooks/useCustomLogin";
+import { signupMember } from "../../api/memberApi";
 
 const LoginModal = ({ isOpen, onClose }) => {
+    const [isLogin, setIsLogin] = useState(true); // true면 로그인, false면 회원가입
     const [loginParam, setLoginParam] = useState({ email: '', password: '', keepLoggedIn: false });
+    const [signupParam, setSignupParam] = useState({ email: '', password: '', nickname: '', role: 'USER' });
     const [errors, setErrors] = useState({ email: '', password: '' });
     const { doLogin, moveToPath } = useCustomLogin();
 
     const emailRegEx = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
     const passwordRegEx = /^[A-Za-z0-9]{8,20}$/;
 
+    // 입력 핸들러
     const handleChange = (e) => {
-        const { name, value, type, checked } = e.target;
-        setLoginParam(prevState => ({
-            ...prevState,
-            [name]: type === 'checkbox' ? checked : value,
-        }));
+        const { name, value } = e.target;
+        if (isLogin) {
+            setLoginParam(prevState => ({
+                ...prevState,
+                [name]: value,
+            }));
+        } else {
+            setSignupParam(prevState => ({
+                ...prevState,
+                [name]: value,
+            }));
+        }
 
-        // 입력값 변화 시 에러 초기화
         setErrors(prevErrors => ({
             ...prevErrors,
             [name]: ''
         }));
     };
 
-    const validate = () => {
+    // 로그인 유효성 검사
+    const validateLogin = () => {
         let isValid = true;
         const newErrors = { email: '', password: '' };
 
@@ -41,9 +52,34 @@ const LoginModal = ({ isOpen, onClose }) => {
         return isValid;
     };
 
+    // 회원가입 유효성 검사
+    const validateSignup = () => {
+        let isValid = true;
+        const newErrors = { email: '', password: '', nickname: '' };
+
+        if (!emailRegEx.test(signupParam.email)) {
+            newErrors.email = '유효한 이메일 주소를 입력하세요.';
+            isValid = false;
+        }
+
+        if (!passwordRegEx.test(signupParam.password)) {
+            newErrors.password = '비밀번호가 유효하지 않습니다.';
+            isValid = false;
+        }
+
+        if (!signupParam.nickname) {
+            newErrors.nickname = '닉네임을 입력하세요.';
+            isValid = false;
+        }
+
+        setErrors(newErrors);
+        return isValid;
+    };
+
+    // 로그인 버튼 클릭 핸들러
     const handleClickLogin = () => {
-        if (!validate()) {
-            return; // 유효성 검사 실패 시 로그인 진행 중단
+        if (!validateLogin()) {
+            return;
         }
 
         doLogin(loginParam).then(data => {
@@ -55,19 +91,37 @@ const LoginModal = ({ isOpen, onClose }) => {
             } else {
                 alert("다시 오셨군요! 환영합니다.");
                 moveToPath('/');
-                onClose(); // 로그인 성공 후 모달 닫기
+                onClose();
             }
-        }); 
+        });
     };
 
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            handleClickLogin();
+    // 회원가입 버튼 클릭 핸들러
+    const handleClickSignup = async () => {
+        if (!validateSignup()) {
+            return;
+        }
+
+        try {
+            await signupMember(signupParam);
+            alert('회원가입이 완료되었습니다. 로그인해주세요.');
+            setIsLogin(true); // 회원가입 후 로그인 화면으로 돌아가기
+        } catch (error) {
+            setErrors({
+                email: error.response?.data?.error === "EMAIL_ALREADY_EXISTS" ? "이미 등록된 이메일입니다." : '',
+                password: '',
+                nickname: ''
+            });
         }
     };
 
     const confirmClose = () => {
         if (window.confirm("진행상황이 저장되지 않습니다. 정말로 닫으시겠습니까?")) {
+            // 상태 초기화
+            setIsLogin(true); // 로그인 상태로 돌아가기
+            setLoginParam({ email: '', password: '', keepLoggedIn: false }); // 로그인 폼 초기화
+            setSignupParam({ email: '', password: '', nickname: '', role: 'USER'}); // 회원가입 폼 초기화
+            setErrors({ email: '', password: '', nickname: '' }); // 에러 메시지 초기화
             onClose();
         }
     };
@@ -77,23 +131,21 @@ const LoginModal = ({ isOpen, onClose }) => {
     return (
         <div
             className="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center z-50"
-            onClick={confirmClose} // 모달 외부 클릭 시 확인창 띄우기
+            onClick={confirmClose}
         >
             <div
                 className="bg-white p-8 rounded-lg shadow-xl w-full max-w-md relative"
-                onClick={(e) => e.stopPropagation()} // 모달 내부 클릭 시 이벤트 전파 막기
+                onClick={(e) => e.stopPropagation()}
             >
-                {/* 오른쪽 상단 닫기 버튼 */}
                 <button className="absolute top-4 right-4" onClick={confirmClose}>
                     <span className="text-3xl font-bold text-gray-400 hover:text-gray-600">&times;</span>
                 </button>
 
-                {/* 로그인 모달 타이틀 */}
                 <div className="text-center mb-6">
-                    <h2 className="text-3xl font-bold text-black">로그인</h2>
+                    <h2 className="text-3xl font-bold text-black">{isLogin ? "로그인" : "회원가입"}</h2>
                 </div>
 
-                {/* 이메일 입력 */}
+                {/* 로그인 또는 회원가입 폼 */}
                 <div className="mb-4">
                     <label className="block text-sm font-semibold mb-2" htmlFor="email">
                         Email
@@ -102,16 +154,14 @@ const LoginModal = ({ isOpen, onClose }) => {
                         id="email"
                         name="email"
                         type="text"
-                        value={loginParam.email}
-                        placeholder={"이메일을 입력하세요."}
+                        value={isLogin ? loginParam.email : signupParam.email}
+                        placeholder="이메일을 입력하세요."
                         onChange={handleChange}
-                        onKeyPress={handleKeyPress} // Enter 키 입력 감지
                         className={`w-full p-2 border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-black`}
                     />
                     {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email}</p>}
                 </div>
 
-                {/* 패스워드 입력 */}
                 <div className="mb-4">
                     <label className="block text-sm font-semibold mb-2" htmlFor="password">
                         Password
@@ -120,39 +170,57 @@ const LoginModal = ({ isOpen, onClose }) => {
                         id="password"
                         name="password"
                         type="password"
-                        placeholder={"비밀번호를 입력하세요."}
-                        value={loginParam.password}
+                        value={isLogin ? loginParam.password : signupParam.password}
+                        placeholder="비밀번호를 입력하세요."
                         onChange={handleChange}
-                        onKeyPress={handleKeyPress} // Enter 키 입력 감지
                         className={`w-full p-2 border ${errors.password ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-black`}
                     />
                     {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password}</p>}
                 </div>
 
-                {/* 로그인 유지하기 체크박스 */}
-                <div className="mb-4 flex items-center">
-                    <input
-                        id="keepLoggedIn"
-                        name="keepLoggedIn"
-                        type="checkbox"
-                        checked={loginParam.keepLoggedIn}
-                        onChange={handleChange}
-                        className="mr-2"
-                    />
-                    <label htmlFor="keepLoggedIn" className="text-sm">
-                        로그인 유지하기
-                    </label>
-                </div>
+                {!isLogin && (
+                    <div className="mb-4">
+                        <label className="block text-sm font-semibold mb-2" htmlFor="nickname">
+                            Nickname
+                        </label>
+                        <input
+                            id="nickname"
+                            name="nickname"
+                            type="text"
+                            value={signupParam.nickname}
+                            placeholder="닉네임을 입력하세요."
+                            onChange={handleChange}
+                            className={`w-full p-2 border ${errors.nickname ? 'border-red-500' : 'border-gray-300'} rounded-lg focus:outline-none focus:ring-2 focus:ring-black`}
+                        />
+                        {errors.nickname && <p className="text-red-500 text-sm mt-1">{errors.nickname}</p>}
+                    </div>
+                )}
 
-                {/* 로그인 버튼 */}
-                <div className="mb-6">
+                <div className="mb-2">
                     <button
                         className="w-full bg-black text-white py-3 rounded-lg hover:bg-gray-800 transition"
-                        onClick={handleClickLogin}
+                        onClick={isLogin ? handleClickLogin : handleClickSignup}
                     >
-                        로그인
+                        {isLogin ? "로그인" : "회원가입"}
                     </button>
                 </div>
+
+                {/* 로그인 유지하기 체크박스 */}
+                {isLogin && (
+                    <div className="mb-4 flex items-center">
+                        <input
+                            id="keepLoggedIn"
+                            name="keepLoggedIn"
+                            type="checkbox"
+                            checked={loginParam.keepLoggedIn}
+                            onChange={handleChange}
+                            className="mr-2"
+                        />
+                        <label htmlFor="keepLoggedIn" className="text-sm">
+                            로그인 유지하기
+                        </label>
+                    </div>
+                )}
 
                 {/* 또는 다른 계정으로 로그인 */}
                 <div className="text-center text-gray-400 mb-5">또는</div>
@@ -172,16 +240,17 @@ const LoginModal = ({ isOpen, onClose }) => {
                     </button>
                 </div>
 
-                {/* 회원가입 링크 */}
                 <div className="text-center mt-6">
                     <p className="text-gray-600">
-                        아직 계정이 없으신가요?{' '}
-                        <a
-                            href="/member/signup"
-                            className="text-blue-500 hover:underline"
+                        {isLogin
+                            ? "아직 계정이 없으신가요? "
+                            : "계정이 있으신가요? "}
+                        <span
+                            onClick={() => setIsLogin(!isLogin)}
+                            className="text-blue-500 hover:underline cursor-pointer"
                         >
-                            회원가입
-                        </a>
+                            {isLogin ? "회원가입" : "로그인"}
+                        </span>
                     </p>
                 </div>
             </div>
