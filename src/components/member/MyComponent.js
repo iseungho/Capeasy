9fd4ca0102e5f1a07c92aba18af90765a1dc1462
iseungho <ghehf51@naver.com>
2024-10-08 16:39:19 +0900

    fix: css수정
    blue -> gray

--- src/components/member/MyComponent.js
+++ src/components/member/MyComponent.js
@@ -128,7 +128,7 @@ const MyComponent = ({mno}) => {
                         </div>
                     </div>
                     <button
-                        className="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-blue-600 transition-colors"
+                        className="px-4 py-2 bg-gray-400 text-white rounded-md hover:bg-gray-600 transition-colors"
                         onClick={moveToModify}
                     >
                         회원정보 수정
