cd73ff9ed2af50f0409e3dbe3f4af24675ae8b00
iseungho <ghehf51@naver.com>
2024-10-19 03:28:03 +0900

    feat: .env추가
    .env를 이용하여 보안과 유지보수에 용이하게 수정

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://127.0.0.1:8080'
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || 'http://locasdfsdflhost:8080';
 const API_SERVER_HOST = `${SERVER_URL}/api`;
 
 export default API_SERVER_HOST;
